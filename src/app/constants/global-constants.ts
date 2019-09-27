aaae8e082bf1cfcb1ded6807d65a33a50a9cda6b
brobertson93 <51289195+brobertson93@users.noreply.github.com>
2019-09-26 21:07:33 -0400

    added fade in animation
    added to Data/Contact

--- src/app/constants/global-constants.ts
+++ src/app/constants/global-constants.ts
@@ -4,7 +4,7 @@ export class GlobalConstants {
     static headerLinks: Link[] = [
         new Link('Resume', '/resume'),
         new Link('Sudoku', '/sudoku'),
+        new Link('Data Display', '/data_display'),
         new Link('Contact', '/contact'),
-        new Link('Data Display', '/data_display')
     ];
 }
